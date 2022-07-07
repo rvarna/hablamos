43ef6319f41ca3c05b846c6809c90516af7fbc1a
Rakesh Varna <rakeshvarna@gmail.com>
2022-07-06 19:18:00 -0700

    Initial set of changes, working to an extent.

---
+++ Hablamos/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Hablamos;
+using Hablamos.Utils;
+
+Console.WriteLine("Welcome to Hablmos, press q to quit anytime.");
+
+QuestionsFactory factory = new QuestionsFactory();
+var question = factory.GetNextQuestion();
+var options = question.GetOptions();
+do
+{
+    
+    Console.WriteLine(question.Verb);
+    for (int i = 0; i < options.Count; i++)
+    {
+        Console.WriteLine($"{i}: {options[i]}");
+    }
+
+    var input = Console.ReadLine();
+    if (input == "q")
+    {
+        Console.WriteLine("Exiting...");
+        return;
+    }
+
+    if (int.TryParse(input, out int guess) && guess < options.Count)
+    {
+        if (options[guess] == question.Translation)
+        {
+            Console.WriteLine("Correct!");
+        }
+        else
+        {
+            Console.WriteLine($"Incorrect. Answer is {question.Translation}");
+        }
+
+        question = factory.GetNextQuestion();
+        options = question.GetOptions();
+    }
+    else
+    {
+        Console.WriteLine($"Invalid guess, try again.");
+    }
+}
+while (true);
---
+++ Hablamos/QuestionsFactory.cs
@@ -0,0 +1,51 @@
+﻿using Hablamos.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hablamos
+{
+    internal class QuestionsFactory
+    {
+        private Dictionary<string, List<string>> _verbsBank = new Dictionary<string, List<string>>();
+
+        private string _previousVerb;
+
+        public QuestionsFactory()
+        {
+            var lines = File.ReadAllLines(@".\lang\es\Verbs.csv");
+            foreach (var line in lines)
+            {
+                var parts = line.Split(',');
+                var verb = parts[0].Trim();
+                var translation = parts[1].Trim();
+
+                if (_verbsBank.TryGetValue(verb, out var translations))
+                {
+                    translations.Add(translation);
+                }
+                else
+                {
+                    _verbsBank[verb] = new List<string> { translation };
+                }
+            }
+        }
+
+        public Question GetNextQuestion()
+        {
+            var options = new List<string>();
+            string verb = _verbsBank.Keys.ToList().PickRandom();
+            var translation = _verbsBank[verb].PickRandom();
+            _verbsBank.Remove(verb);
+            while (options.Count < 3)
+            {
+                var option = _verbsBank.Keys.ToList().PickRandom();
+                options.Add(_verbsBank[option].PickRandom());                
+            }
+
+            return new Question(verb, translation, options);
+        }
+    }
+}
